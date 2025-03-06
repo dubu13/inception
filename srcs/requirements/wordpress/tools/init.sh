e510edff0a6f74f883c0fe6a774bee2a64bf21ad
dubu13 <dilara.hasan1301@gmail.com>
2025-03-06 16:17:17 +0100

    wordpess/script cleaned up, nginx conf fixed

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -2,42 +2,37 @@
 
 # reading secrets
 MYSQL_PASSWORD=$(cat /run/secrets/db_user_password)
-# WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
-# WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
+WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
-# waiting for mysql to be ready
-while ! mysql -h mariadb -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} >/dev/null 2>&1; do
-    echo "Waiting for MariaDB..."
-    sleep 1
-done
 
-# geting authentication keys
-KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+if [ ! -f "wp-config.php" ]; then
+    echo "Create wp-config.php"
+    wp core download --allow-root --path='/var/www/html'
+    wp config create --allow-root \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=mariadb:3306 \
+        --path='/var/www/html'
 
-# creating new wp-config.php
-cat > /var/www/html/wordpress/wp-config.php << EOF
-<?php
-define( 'DB_NAME', '${MYSQL_DATABASE}' );
-define( 'DB_USER', '${MYSQL_USER}' );
-define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );
-define( 'DB_HOST', 'mariadb' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+    wp core install --allow-root \
+        --url="https://${DOMAIN_NAME}" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="admin@example.com" \
+        --skip-email
 
-$KEYS
+    echo "Create WordPress user"
+    wp user create --allow-root \
+        dhasan admin2@example.com \
+        --role=author \
+        --user_pass="$WP_USER_PASSWORD"
+fi
 
-\$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
+echo "Ensure PHP run directory exists"
+mkdir -p /run/php
 
-if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
-}
-
-require_once ABSPATH . 'wp-settings.php';
-EOF
-
-# update ownership
-chown www-data:www-data /var/www/html/wordpress/wp-config.php
-# start php-fpm
-echo "Starting PHP-FPM..."
+echo "Start PHP-FPM"
 exec php-fpm7.4 -F
